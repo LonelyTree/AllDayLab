2e4acfa75307679bb14b6031527bdf735de74300
Alex Hughes <alexhugh121@gmail.com>
2019-04-15 12:51:58 -0700

    Added Server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express.route();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(methodOverride('_method'))
+
+
+
+app.listen(3000, () => {
+    console.log('app listening on port: ', 3000);
+});
