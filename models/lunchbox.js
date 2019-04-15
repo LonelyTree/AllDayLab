9bfc9b648248cc7f65333f9bd8c6a561e68d6e59
Alex Hughes <alexhugh121@gmail.com>
2019-04-15 11:30:20 -0700

    Added Model

---
+++ models/lunchbox.js
@@ -0,0 +1,20 @@
+const lunchBox = [
+    {
+        name: "Alex",
+        color: "Green",
+        food: "Boudin",
+        drink: "Water"
+    },
+    {
+        name: "Josh",
+        color: "Grey",
+        food: "Sushi",
+        drink: "Dr. Pepper"
+    },
+    {
+        name: "Ethan",
+        color: "Red",
+        food: "Sandwich",
+        drink: "Lemonade"
+    },
+]
