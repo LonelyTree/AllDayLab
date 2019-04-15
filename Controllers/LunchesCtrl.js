857f98794cdd8cc5ec966d4afe1c5e8211375ea0
Alex Hughes <alexhugh121@gmail.com>
2019-04-15 14:50:02 -0700

    updated Controller

--- Controllers/LunchesCtrl.js
+++ Controllers/LunchesCtrl.js
@@ -1,13 +1,28 @@
 const Lunches = require('../models/lunchbox')
 
 
-const index = (req, res) => {
+const get = (req, res) => {
     res.render('index.ejs', {
         lunches: Lunches,
     })
 }
 
+const put = (req, res) => {
+    req.params.id = req.body
+    res.redirect('index.ejs', {
+        lunches: Lunches[req.params.id],
+    })
+}
+
+const del = (req, res) => {
+    Lunches.splice(req.params.id, 1)
+    res.render('index.ejs', {
+        lunches: Lunches
+    });
+}
 
 module.exports = {
-    index
+    get,
+    put,
+    del
 }
