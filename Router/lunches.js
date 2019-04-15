19c4175026e85818712c57efc74ee8e1e7c7d503
Alex Hughes <alexhugh121@gmail.com>
2019-04-15 14:16:11 -0700

    updated Controller and Router

--- Router/lunches.js
+++ Router/lunches.js
@@ -1,23 +1,20 @@
 const express=require ('express')
 const router = express.Router();
-const Lunches=require('../models/lunchbox')
+const Lunches = require('../models/lunchbox')
+const lunchCtrl = require('../Controllers/LunchesCtrl.js')
 
-const get = function () {
-  router.get('/', (req, res) => {
-    res.render('index.ejs', {
-      lunches: Lunches
-    });
-  });
-}
 
-const del = function () {
+  router.get('/', lunchCtrl.index);
+
+
+
   router.delete('/:id', (req, res) => {
     Lunches.splice(req.params.id, 1)
     res.render('index.ejs', {
       lunches: Lunches
     });
   });
-}
+
 
 
 
--- models/lunchbox.js
+++ models/lunchbox.js
@@ -18,3 +18,5 @@ const lunchBox = [
         drink: "Lemonade"
     },
 ]
+
+module.exports = lunchBox
