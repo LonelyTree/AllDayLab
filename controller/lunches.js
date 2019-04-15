3ddead45e40ada584d0b2387ba56162ff1ac9f1e
Ethan <N/A>
2019-04-15 14:36:20 -0700

    fff

--- controller/lunches.js
+++ controller/lunches.js
@@ -2,22 +2,22 @@ const express=require ('express')
 const router = express.Router();
 const Lunches=require('../models/lunchbox.js')
 
-const get = function () {
+
   router.get('/', (req, res) => {
     res.render('index.ejs', {
       lunches: Lunches
     });
   });
-}
 
-const del = function () {
+
+
   router.delete('/:id', (req, res) => {
     Lunches.splice(req.params.id, 1)
     res.render('index.ejs', {
       lunches: Lunches
     });
   });
-}
+
 
 
 
